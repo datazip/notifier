eff8e555d1dfedef62dab6bd48330e03412da296
Piyush Singariya <piyushsingariya@datazip.io>
2022-12-21 11:09:38 +0530

    okay if notifier isn't initialized

--- notifier.go
+++ notifier.go
@@ -2,30 +2,45 @@ package notifier
 
 // Notify error logs on Slack
 func NotifyError(errorAt, description, errString string, fields ...string) {
-	standardNotifier.NotifyError(errorAt, description, errString, fields...)
+	if standardNotifier != nil {
+		standardNotifier.NotifyError(errorAt, description, errString, fields...)
+	}
 }
 
 // Notify success logs on Slack
 func NotifySuccess(successAt, description, successString string, fields ...string) {
-	standardNotifier.NotifySuccess(successAt, description, successString, fields...)
+	if standardNotifier != nil {
+		standardNotifier.NotifySuccess(successAt, description, successString, fields...)
+	}
 }
 
 // Notify success logs on Slack
 func NotifyWarn(warnAt, description, warnString string, fields ...string) {
-	standardNotifier.NotifyWarn(warnAt, description, warnString, fields...)
+	if standardNotifier != nil {
+		standardNotifier.NotifyWarn(warnAt, description, warnString, fields...)
+	}
 }
 
 // Notify error logs on Slack
 func NotifyErrorE(errorAt, description, errString string, fields ...string) error {
-	return standardNotifier.NotifyErrorE(errorAt, description, errString, fields...)
+	if standardNotifier != nil {
+		return standardNotifier.NotifyErrorE(errorAt, description, errString, fields...)
+	}
+	return nil
 }
 
 // Notify success logs on Slack
 func NotifySuccessE(successAt, description, successString string, fields ...string) error {
-	return standardNotifier.NotifySuccessE(successAt, description, successString, fields...)
+	if standardNotifier != nil {
+		return standardNotifier.NotifySuccessE(successAt, description, successString, fields...)
+	}
+	return nil
 }
 
 // Notify success logs on Slack
 func NotifyWarnE(warnAt, description, warnString string, fields ...string) error {
-	return standardNotifier.NotifyWarnE(warnAt, description, warnString, fields...)
+	if standardNotifier != nil {
+		return standardNotifier.NotifyWarnE(warnAt, description, warnString, fields...)
+	}
+	return nil
 }
